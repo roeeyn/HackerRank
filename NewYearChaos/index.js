b055c67e83472a5d1bb60bcdc3b6798adeae5c69
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-10-23 23:46:56 -0500

    Improved algorithm, passed technical tests but large tests timeout

--- NewYearChaos/index.js
+++ NewYearChaos/index.js
@@ -1,4 +1,4 @@
-module.exports = queue => {
+module.exports = originalQueue => {
   const checkQueue = queue =>
     queue.reduce(
       (carry, el, idx, originalQ) =>
@@ -9,64 +9,33 @@ module.exports = queue => {
     );
 
   try {
-    const [semiQueue, initialBribeCounts] = queue.reduce(
-      (carry, element, idx) => {
-        if (idx === queue.length - 1) return carry;
+    const getBribesSum = (queue, bribeCount = 0, bribedNumbers = {}) => {
+      const [newQueue, newBribeCounts, newBribedNumbers] = queue.reduce(
+        (carry, element, idx) => {
+          if (idx === queue.length - 1) return carry;
 
-        const [newQueue, bribeCounts, bribedNumbers] = carry;
-        const [leftEl, rightEl] = newQueue.slice(idx, idx + 2);
-        if (leftEl > rightEl) {
-          newQueue[idx] = rightEl;
-          newQueue[idx + 1] = leftEl;
-          const prevBribeCount = bribedNumbers[leftEl] || 0;
-          if (prevBribeCount === 2) throw new Error("Too chaotic");
-          bribedNumbers[leftEl] = prevBribeCount + 1;
-          return [[...newQueue], bribeCounts + 1, bribedNumbers];
-        }
-        return carry;
-      },
-      [[...queue], 0, {}] // [newQueue, bribeCounts, bribedNumbers]
-    );
+          const [newQueue, bribeCounts, bribedNumbers] = carry;
+          const [leftEl, rightEl] = newQueue.slice(idx, idx + 2);
+          if (leftEl > rightEl) {
+            newQueue[idx] = rightEl;
+            newQueue[idx + 1] = leftEl;
+            const prevBribeCount = bribedNumbers[leftEl] || 0;
+            if (prevBribeCount === 2) throw new Error("Too chaotic");
+            bribedNumbers[leftEl] = prevBribeCount + 1;
+            return [[...newQueue], bribeCounts + 1, bribedNumbers];
+          }
+          return carry;
+        },
+        [[...queue], bribeCount, bribedNumbers] // [newQueue, bribeCounts, bribedNumbers]
+      );
 
-    const reversedQueue = semiQueue.reverse();
-    const [newReversedQueue, reversedBribeCounts] = reversedQueue.reduce(
-      (carry, element, idx) => {
-        if (idx === queue.length - 1) return carry;
-        const [newQueue, bribeCounts] = carry;
-        const [leftEl, rightEl] = newQueue.slice(idx, idx + 2);
-        if (leftEl < rightEl) {
-          newQueue[idx] = rightEl;
-          newQueue[idx + 1] = leftEl;
-          return [[...newQueue], bribeCounts + 1];
-        }
-        return carry;
-      },
-      [[...reversedQueue], 0]
-    ); // [newQueue, bribeCounts]
+      return checkQueue(newQueue)
+        ? newBribeCounts
+        : getBribesSum([...newQueue], newBribeCounts, newBribedNumbers);
+    };
 
-    const [newReversedQueue2, reversedBribeCounts2] = newReversedQueue.reduce(
-      (carry, element, idx) => {
-        if (idx === queue.length - 1) return carry;
-        const [newQueue, bribeCounts] = carry;
-        const [leftEl, rightEl] = newQueue.slice(idx, idx + 2);
-        if (leftEl < rightEl) {
-          newQueue[idx] = rightEl;
-          newQueue[idx + 1] = leftEl;
-          return [[...newQueue], bribeCounts + 1];
-        }
-        return carry;
-      },
-      [[...newReversedQueue], 0]
-    ); // [newQueue, bribeCounts]
-
-    const result =
-      reversedBribeCounts2 + reversedBribeCounts + initialBribeCounts;
-    return result;
+    return getBribesSum(originalQueue);
   } catch (e) {
     return e.message;
   }
 };
-
-// console.log(7, module.exports([1, 2, 5, 3, 7, 8, 6, 4]));
-// console.log(checkQueue([1, 2, 3, 4]));
-// console.log(checkQueue([1, 3, 2, 4]));
