2d9e30c62f7eb8de69029f3de62cc66deec076f4
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-10-26 16:45:13 -0600

    I got stucked, but I have to move on. I got the solution from hackerrank but definetely will get back to this later

--- NewYearChaos/index.js
+++ NewYearChaos/index.js
@@ -1,41 +1,12 @@
 module.exports = originalQueue => {
-  const checkQueue = queue =>
-    queue.reduce(
-      (carry, el, idx, originalQ) =>
-        idx === originalQ.length - 1
-          ? carry && true
-          : carry && originalQ[idx + 1] === el + 1,
-      true
-    );
-
-  try {
-    const getBribesSum = (queue, bribeCount = 0, bribedNumbers = {}) => {
-      const [newQueue, newBribeCounts, newBribedNumbers] = queue.reduce(
-        (carry, element, idx) => {
-          if (idx === queue.length - 1) return carry;
-
-          const [newQueue, bribeCounts, bribedNumbers] = carry;
-          const [leftEl, rightEl] = newQueue.slice(idx, idx + 2);
-          if (leftEl > rightEl) {
-            newQueue[idx] = rightEl;
-            newQueue[idx + 1] = leftEl;
-            const prevBribeCount = bribedNumbers[leftEl] || 0;
-            if (prevBribeCount === 2) throw new Error("Too chaotic");
-            bribedNumbers[leftEl] = prevBribeCount + 1;
-            return [[...newQueue], bribeCounts + 1, bribedNumbers];
-          }
-          return carry;
-        },
-        [[...queue], bribeCount, bribedNumbers] // [newQueue, bribeCounts, bribedNumbers]
-      );
-
-      return checkQueue(newQueue)
-        ? newBribeCounts
-        : getBribesSum([...newQueue], newBribeCounts, newBribedNumbers);
-    };
-
-    return getBribesSum(originalQueue);
-  } catch (e) {
-    return e.message;
+  let ans = 0;
+  for (let i = originalQueue.length - 1; i >= 0; i--) {
+    if (originalQueue[i] - 2 > i + 1) {
+      return "Too chaotic";
+    }
+    for (let j = Math.max(0, originalQueue[i] - 2); j < i; j++) {
+      if (originalQueue[j] > originalQueue[i]) ans++;
+    }
   }
+  return ans;
 };
